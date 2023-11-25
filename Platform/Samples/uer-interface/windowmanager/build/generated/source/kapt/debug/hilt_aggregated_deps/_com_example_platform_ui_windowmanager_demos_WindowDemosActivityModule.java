58648a21ea49cec6998c9b94e605f9a84e8d1ddb
plticmd <86671100+plticmd@users.noreply.github.com>
2023-11-25 09:23:06 +0900

    Add files via upload

---
+++ Platform/Samples/uer-interface/windowmanager/build/generated/source/kapt/debug/hilt_aggregated_deps/_com_example_platform_ui_windowmanager_demos_WindowDemosActivityModule.java
@@ -0,0 +1,15 @@
+package hilt_aggregated_deps;
+
+import dagger.hilt.processor.internal.aggregateddeps.AggregatedDeps;
+import javax.annotation.processing.Generated;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedDeps(
+    components = "dagger.hilt.components.SingletonComponent",
+    modules = "com.example.platform.ui.windowmanager.demos.WindowDemosActivityModule"
+)
+@Generated("dagger.hilt.processor.internal.aggregateddeps.AggregatedDepsGenerator")
+public class _com_example_platform_ui_windowmanager_demos_WindowDemosActivityModule {
+}
